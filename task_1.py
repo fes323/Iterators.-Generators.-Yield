b2e16651146b00bbae289df49a41a563584ca73d
Maksim legeyda <Maksim.legeyda@yandex.ru>
2022-11-11 23:52:12 +0300

    1 task

--- task_1.py
+++ task_1.py
@@ -1,18 +1,26 @@
 class FlatIterator:
 
     def __init__(self, list_of_list):
-        ...
+        self.list_of_list = list_of_list
+        self.counter = -1
+        self.nest_counter = 0
+        self.limit = len(self.list_of_list)
 
     def __iter__(self):
-        ...
+        self.counter += 1
+        self.nest_counter = 0
         return self
 
     def __next__(self):
-        ...
+        while self.counter - self.limit and self.nest_counter == len(self.list_of_list[self.counter]):
+            iter(self)
+        if self.counter == self.limit:
+            raise StopIteration
+        self.nest_counter += 1
+        item = self.list_of_list[self.counter][self.nest_counter - 1]
         return item
 
-
-def test():
+def test_1():
 
     list_of_lists_1 = [
         ['a', 'b', 'c'],
@@ -24,11 +32,12 @@ def test():
             FlatIterator(list_of_lists_1),
             ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
     ):
-
+        print(flat_iterator_item)
+        print(check_item)
         assert flat_iterator_item == check_item
 
     assert list(FlatIterator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
 
 
 if __name__ == '__main__':
-    test()
+    test_1()
